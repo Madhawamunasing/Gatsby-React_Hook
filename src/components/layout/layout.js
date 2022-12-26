dec972212565ca4810416e51acc19790096099e7
Madhawa Munasinghe <madhawaprageeth86@gmail.com>
2022-12-26 23:33:26 +0530

    file changes

---
+++ src/components/layout/layout.js
@@ -0,0 +1,27 @@
+import * as React from "react"
+// import { useStaticQuery, graphql } from "gatsby"
+
+// import Header from "./header"
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
+
+  return (
+    <>
+      <div>
+        <main>{children}</main>
+      </div>
+    </>
+  )
+}
+
+export default Layout
