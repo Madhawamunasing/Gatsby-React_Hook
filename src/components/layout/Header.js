79bd0ea3a92146e145b421eaed31ceb32ce52334
Madhawa Munasinghe <madhawaprageeth86@gmail.com>
2022-12-28 16:22:53 +0530

    header changes

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 import { menuData } from "../../data/menuData"
@@ -7,16 +7,37 @@ import MenuToolTip from "../toolTip/menuToolTip"
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false)
+  const ref = useRef()
+  const tooltipRef = useRef()
+
   function handleClick(event) {
     setIsOpen(!isOpen)
     event.preventDefault()
   }
+
+  function handleClickOutside(event) {
+    if (
+      ref.current &&
+      ref.current.contains(event.target) &&
+      !tooltipRef.current.contains(event.target)
+    ) {
+      console.log("Document is clicked")
+      setIsOpen(false)
+    }
+  }
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [])
+
   return (
     <Wrapper>
       <Link to="/">
         <img src="/images/logos/logo.svg" alt="logo" />
       </Link>
-      <MenuWrapper count={menuData.length}>
+      <MenuWrapper count={menuData.length} ref={ref}>
         {menuData.map((item, index) =>
           item.link === "/account" ? (
             <MenuButton
@@ -30,11 +51,14 @@ function Header() {
         )}
         <HamburgerWrapper>
           <MenuButton
-            item={{ title: "", icon: "/images/icons/hamburger.svg", link: "" }}
+            item={{ title: "", icon: "/images/icons/hamburger.svg", link: "/" }}
+            onClick={event => handleClick(event)}
           />
         </HamburgerWrapper>
       </MenuWrapper>
-      <MenuToolTip isOpen={isOpen} />
+      <div ref={tooltipRef}>
+        <MenuToolTip isOpen={isOpen} />
+      </div>
     </Wrapper>
   )
 }
